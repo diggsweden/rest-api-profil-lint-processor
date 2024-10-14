5d0a10da3033c247c4da34c80a99d0a740fb0f8c
Fredrik Nordlander <fredrik.nordlander@digg.se>
2024-10-14 14:17:56 +0200

    Lagt till funktionalitet för att köra RAP-LP i olika modes(CLI,API)-MVP2

--- src/api-mode.ts
+++ src/api-mode.ts
@@ -10,6 +10,27 @@ export function startServer() {
     res.send('API-läge aktiverat! Validerar YAML-filer här...');
   });
 
+  /*
+        Open API schema för restinterfacet för RAP-LP
+  */
+    /*      - Gemensam funktionalitet?
+            - Funktion för att invoka nuvarande kodbas för regelmotorn 
+            - Funktion för att leverera resultat från Diagnostiseringen 
+
+    */
+  /*	Möjlighet till att specificera en URL för att kunna 
+        peka ut en OpenApi Specifikation v. >3.0  för validering
+        - Konstruera en enpoint för detta  (Routes också)
+        - Funktion för att invoka URL(tredjeparts lib(typ axios)) (Medium) inkl. validering etc.
+  */
+  /*	Möjlighet till att specificera en fil för att kunna 
+        peka ut en OpenApi Specifikation v. >3.0  för validering
+        - Konstruera en enpoint för detta  (Routes också)
+
+    	- Möjlighet till att specificera content för en 
+        - Konstruera en enpoint för detta  (Routes också)
+          OpenApi Specifikation v. >3.0 för validering 
+  */
   // Starta servern
   app.listen(port, () => {
     console.log(`Servern körs på http://localhost:${port}`);
