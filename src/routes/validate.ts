6a81aa190d9b01f67b1eed48abd7c4cb9bdd5862
Pontus Ellström <extern.pontus.ellstrom@digg.se>
2024-10-21 12:41:02 +0200

    feat: Add try catch for error handling for route validate/file

--- src/routes/validate.ts
+++ src/routes/validate.ts
@@ -5,11 +5,13 @@ import multer from 'multer'
 import { ERROR_TYPE, RapLPBaseApiError } from "../util/RapLPBaseApiError.ts"
 import { processApiSpec, validateYamlInput } from "../util/apiUtil.ts"
 
-export const registerValidationRoutes = (app: Express, enabledRulesAndCategorys: { rules: Record<string, any>,
-    instanceCategoryMap: Map<string, any> }) => {
+export const registerValidationRoutes = (app: Express, enabledRulesAndCategorys: {
+    rules: Record<string, any>,
+    instanceCategoryMap: Map<string, any>
+}) => {
 
     // Route based on content-type, in order to support file upload.
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         if (req.url === "/api/v1/validate/content" && req.headers["content-type"]?.includes("multipart/form-data")) {
             req.url = "/api/v1/validate/file"
         }
@@ -21,7 +23,7 @@ export const registerValidationRoutes = (app: Express, enabledRulesAndCategorys:
         const rawInput = req.body;
 
         try {
-            if(!validateYamlInput(rawInput)) {
+            if (!validateYamlInput(rawInput)) {
                 next(new RapLPBaseApiError("Kunde inte parsa YAML filen.", ERROR_TYPE.BAD_REQUEST));
                 return
             }
@@ -45,25 +47,29 @@ export const registerValidationRoutes = (app: Express, enabledRulesAndCategorys:
     // Route for uploading file through standard multipart/form-data.
     app.post("/api/v1/validate/file", upload.single("file"), async (req, res, next) => {
 
-        const file = req.file;
-        if (!file) {
-            next(new RapLPBaseApiError("Kunde inte ladda upp fil.", ERROR_TYPE.INTERNAL_SERVER_ERROR))
-            return
+        try {
+            const file = req.file;
+            if (!file) {
+                next(new RapLPBaseApiError("Kunde inte ladda upp fil.", ERROR_TYPE.INTERNAL_SERVER_ERROR))
+                return
+            }
+
+            const fileContent = file.buffer.toString()
+            if (!validateYamlInput(fileContent)) {
+                next(new RapLPBaseApiError("Kunde inte parsa YAML filen.", ERROR_TYPE.BAD_REQUEST));
+                return
+            }
+
+            const apiSpecDocument = new Document(
+                fileContent,
+                Parsers.Yaml,
+                file.originalname
+            );
+
+            const result = await processApiSpec(enabledRulesAndCategorys, apiSpecDocument)
+            res.send(result)
+        } catch (e) {
+            next(e)
         }
-
-        const fileContent = file.buffer.toString()
-        if (!validateYamlInput(fileContent)) {
-            next(new RapLPBaseApiError("Kunde inte parsa YAML filen.", ERROR_TYPE.BAD_REQUEST));
-            return
-        }
-
-        const apiSpecDocument = new Document(
-            fileContent,
-            Parsers.Yaml,
-            file.originalname
-        );
-
-        const result = await processApiSpec(enabledRulesAndCategorys, apiSpecDocument)
-        res.send(result)
     })
 }
