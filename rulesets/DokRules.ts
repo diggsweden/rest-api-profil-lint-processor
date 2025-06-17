7748668ae684a43b0122c22be48fa6b875002a42
Mats Johansson <extern.mats.johansson@digg.se>
2025-06-17 09:55:40 +0200

    refactor: replace loops and use regex for word matching
    Signed-off-by: Mats Johansson <extern.mats.johansson@digg.se>

--- rulesets/DokRules.ts
+++ rulesets/DokRules.ts
@@ -18,6 +18,7 @@ import { Dok03Base } from './rulesetUtil.ts';
 import path from 'path';
 import { Dok15Base } from './rulesetUtil.ts';
 import { forEachChild } from 'typescript';
+import { commonEnglishWords, commonSwedishWords } from './constants/CommonWords.ts';
 const moduleName: string = 'DokRules.ts';
 
 export class Dok15Get extends Dok15Base {
@@ -153,27 +154,10 @@ export class Dok06 extends BaseRuleset {
   then = [
     {
       function: (targetVal: string, _opts: string, paths: string[]) => {
-        const englishWords = ['and', 'it']; //TODO: fler ord
-        const swedishWords = ['och', 'det']; //TODO: fler ord
+        const containsEnglish = commonEnglishWords.some((word) => new RegExp(`\\b${word}\\b`).test(targetVal));
+        const containsSwedish = commonSwedishWords.some((word) => new RegExp(`\\b${word}\\b`).test(targetVal));
 
-        let includesEnglish = false;
-        let includesSwedish = false;
-
-        for (const word of englishWords) {
-          if (targetVal.includes(word)) {
-            includesEnglish = true;
-            break;
-          }
-        }
-
-        for (const word of swedishWords) {
-          if (targetVal.includes(word)) {
-            includesSwedish = true;
-            break;
-          }
-        }
-
-        if (!includesEnglish || !includesSwedish) {
+        if (!containsEnglish || !containsSwedish) {
           return [
             {
               message: this.message,
