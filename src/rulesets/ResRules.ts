4d7bb5399ace131153b18f1045632bb49c043fdb
Mats Johansson <extern.mats.johansson@digg.se>
2025-08-14 14:17:27 +0200

    feat: add rule RES.06
    Signed-off-by: Mats Johansson <extern.mats.johansson@digg.se>

--- src/rulesets/ResRules.ts
+++ src/rulesets/ResRules.ts
@@ -56,4 +56,62 @@ export class Res02 extends BaseRuleset {
   }
 }
 
-export default { Res02 };
+export class Res06 extends BaseRuleset {
+  static customProperties: CustomProperties = {
+    område: 'Resurser',
+    id: 'RES.06',
+  };
+  message =
+    'Resurser SKALL följa den namnsättningskonvention som beskrivs för URL:er, det vill säga att resurser anges med gemener, använder endast alfanumeriska tecken och bindestreck för att separera eventuella ord. ';
+  given = '$.paths[*]~';
+  then = [
+    {
+      function: (targetVal: any, _opts: string, paths: string[]) => {
+        const expression = new RegExp('^(?!-)[a-z0-9-]+(?<!-)$');
+        const invalidPrefixes = ['get-', 'post-', 'put-', 'delete-', 'patch-'];
+
+        let invalid = false;
+
+        targetVal.split('/').forEach((part: string) => {
+          if (part.length > 0 && !part.includes('{') && !part.includes('}')) {
+            const hasInvalidPrefix = invalidPrefixes.some((prefix) => part.startsWith(prefix));
+            if (!expression.test(part) || hasInvalidPrefix) {
+              invalid = true;
+            }
+          }
+        });
+
+        if (invalid) {
+          return [
+            {
+              message: this.message,
+              severity: this.severity,
+              paths: paths,
+            },
+          ];
+        }
+        return [];
+      },
+    },
+    {
+      function: (targetVal: string, _opts: string, paths: string[]) => {
+        this.trackRuleExecutionHandler(
+          JSON.stringify(targetVal, null, 2),
+          _opts,
+          paths,
+          this.severity,
+          this.constructor.name,
+          moduleName,
+          Res06.customProperties
+        );
+      },
+    },
+  ];
+  severity = DiagnosticSeverity.Error;
+  constructor() {
+    super();
+    super.initializeFormats(['OAS3']);
+  }
+}
+
+export default { Res02, Res06 };
