e40d63e5317034842353c05bc6b19dbf8269f444
Mats Johansson <extern.mats.johansson@digg.se>
2025-08-15 10:43:09 +0200

    feat: add rule DOK.08, set trailingCommas to true in prettier config
    Signed-off-by: Mats Johansson <extern.mats.johansson@digg.se>

--- rulesets/DokRules.ts
+++ rulesets/DokRules.ts
@@ -214,6 +214,73 @@ export class Dok07 extends BaseRuleset {
   }
   severity = DiagnosticSeverity.Warning;
 }
+export class Dok08 extends BaseRuleset {
+  static customProperties: CustomProperties = {
+    område: 'Dokumentation',
+    id: 'DOK.08',
+  };
+  given = '$';
+  message = 'Ett API:s servicenivå SKALL (DOK.08) finnas tydligt beskriven i dokumentationen.';
+  then = [
+    {
+      function: (targetVal: any, _opts: string, paths: string[]) => {
+        const isValidUrl = (url: string): boolean => {
+          const urlPattern = new RegExp('^(https?://)?(www.)?([a-zA-Z0-9-]+.)+[a-zA-Z]{2,}(/S*)?$');
+          return urlPattern.test(url);
+        };
+
+        const hasValidSlaFields = (sla: any): boolean => {
+          return !!(sla?.availability && sla?.responseTime && sla?.support);
+        };
+
+        if (targetVal?.info?.termsOfService) {
+          return [];
+        }
+
+        const sla = targetVal?.info?.['x-sla'];
+        if (sla && hasValidSlaFields(sla)) {
+          return [];
+        }
+
+        const externalDocs = targetVal?.externalDocs;
+        if (
+          externalDocs?.description &&
+          externalDocs?.url &&
+          isValidUrl(externalDocs.url) &&
+          externalDocs.description.toLowerCase().includes('service level agreement')
+        ) {
+          return [];
+        }
+
+        return [
+          {
+            message: this.message,
+            severity: this.severity,
+            paths: paths,
+          },
+        ];
+      },
+    },
+    {
+      function: (targetVal: string, _opts: string, paths: string[]) => {
+        this.trackRuleExecutionHandler(
+          JSON.stringify(targetVal, null, 2),
+          _opts,
+          paths,
+          this.severity,
+          this.constructor.name,
+          moduleName,
+          Dok07.customProperties,
+        );
+      },
+    },
+  ];
+  constructor() {
+    super();
+    super.initializeFormats(['OAS2', 'OAS3']);
+  }
+  severity = DiagnosticSeverity.Error;
+}
 export class Dok19 extends BaseRuleset {
   static customProperties: CustomProperties = {
     område: 'Dokumentation',
