f9c10f2444048e608bb0b0a50ce4a7c7b446716e
Mats Johansson <56579809+mmjohansson@users.noreply.github.com>
2025-08-26 12:55:00 +0200

    Revert "refactor: convert targetVal to lower case before checks"
    This reverts commit cdd83dc5e019a54423bd0f1787a189fc0701c718.

--- rulesets/DokRules.ts
+++ rulesets/DokRules.ts
@@ -145,9 +145,8 @@ export class Dok06 extends BaseRuleset {
   then = [
     {
       function: (targetVal: string, _opts: string, paths: string[]) => {
-        const lowerCaseTargetVal = targetVal.toLowerCase();
-        const containsEnglish = commonEnglishWords.some((word) => new RegExp(`\\b${word}\\b`).test(lowerCaseTargetVal));
-        const containsSwedish = commonSwedishWords.some((word) => new RegExp(`\\b${word}\\b`).test(lowerCaseTargetVal));
+        const containsEnglish = commonEnglishWords.some((word) => new RegExp(`\\b${word}\\b`).test(targetVal));
+        const containsSwedish = commonSwedishWords.some((word) => new RegExp(`\\b${word}\\b`).test(targetVal));
 
         if (!containsEnglish || !containsSwedish) {
           return [
