4be7e06753cf23f32bcc948f16b81617ab22594d
Mats Johansson <56579809+mmjohansson@users.noreply.github.com>
2025-10-09 08:43:34 +0200

    feat: add rule RES.06
    Signed-off-by: Mats Johansson <extern.mats.johansson@digg.se>

--- tests/unit/res.test.ts
+++ tests/unit/res.test.ts
@@ -97,3 +97,97 @@ testRule('Res02', [
     errors: [],
   },
 ]);
+
+testRule('Res06', [
+  {
+    name: 'ogiltigt testfall - Resurser BÖR INTE innehålla versaler',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0.0' },
+      paths: {
+        '/PETS': {},
+      },
+    },
+    errors: [
+      {
+        message:
+          'Resurser SKALL följa den namnsättningskonvention som beskrivs för URL:er, det vill säga att resurser anges med gemener, använder endast alfanumeriska tecken och bindestreck för att separera eventuella ord.',
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+  {
+    name: 'ogiltigt testfall - Bindestreck i resurser BÖR endast användas för att separera andra ord',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0.0' },
+      paths: {
+        '/-pets': {},
+      },
+    },
+    errors: [
+      {
+        message:
+          'Resurser SKALL följa den namnsättningskonvention som beskrivs för URL:er, det vill säga att resurser anges med gemener, använder endast alfanumeriska tecken och bindestreck för att separera eventuella ord.',
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+  {
+    name: 'ogiltigt testfall - Resurser BÖR inte innehålla underline',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0.0' },
+      paths: {
+        '/my_pets': {},
+      },
+    },
+    errors: [
+      {
+        message:
+          'Resurser SKALL följa den namnsättningskonvention som beskrivs för URL:er, det vill säga att resurser anges med gemener, använder endast alfanumeriska tecken och bindestreck för att separera eventuella ord.',
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+  {
+    name: 'giltigt testfall - Resurser BÖR endast innehålla gemener, siffror, och bindestreck för att separera ord',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0.0' },
+      paths: {
+        '/multiple-words-123': {
+          get: {
+            parameters: [
+              {
+                name: 'title',
+                in: 'path',
+              },
+            ],
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+  {
+    name: 'giltigt testfall - Path params undantas från RES.06 och får därför innehålla versaler',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0.0' },
+      paths: {
+        '/pets/{petId}': {
+          get: {
+            parameters: [
+              {
+                name: 'title',
+                in: 'path',
+              },
+            ],
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+]);
