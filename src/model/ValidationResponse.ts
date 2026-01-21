97f2f989b1cd9153ca56b80072f7394927e52224
Fredrik Nordlander <fredrik.nordlander@digg.se>
2026-01-21 08:23:05 +0100

    fix: add copyright and licensing information
    Signed-off-by: Fredrik Nordlander <fredrik.nordlander@digg.se>

--- src/model/ValidationResponse.ts
+++ src/model/ValidationResponse.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import { ValidationResponseDto } from './ValidationResponseDto.js';
 import { Issue } from '../util/Issue.js';
 
--- src/rulesets/util/rules-doc.config.ts
+++ src/rulesets/util/rules-doc.config.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 const RULES_DOC_REPO =
   'https://github.com/diggsweden/rest-api-profil-lint-processor';
 
