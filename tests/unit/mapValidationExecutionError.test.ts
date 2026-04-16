8b846474633042be4baad24e56f48dd986e3e297
Fredrik Nordlander <fredrik.nordlander@digg.se>
2026-04-16 11:27:27 +0200

    fix: add missing annotations
    Signed-off-by: Fredrik Nordlander <fredrik.nordlander@digg.se>

--- tests/unit/mapValidationExecutionError.test.ts
+++ tests/unit/mapValidationExecutionError.test.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
 import { mapValidationExecutionError } from '../../src/util/mapValidationExecutionError';
 import { SpecParseError } from '../../src/util/RapLPSpecParseError';
 import { AggregateError } from '../../src/util/RapLPCustomErrorInfo.js'
