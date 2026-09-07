0b0a8224d7e63b43e7ed285cd443a31db6a89300
Mats Johansson <extern.mats.johansson@digg.se>
2026-09-07 22:53:53 +0200

    chore: add missing license header
    Signed-off-by: Mats Johansson <extern.mats.johansson@digg.se>

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import { Express } from 'express';
 import { register } from '../metrics.js';
 
