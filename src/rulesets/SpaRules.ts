b4f12cb08ca6bf87f1856bf5e08adc424cd13c6a
Mats Johansson <extern.mats.johansson@digg.se>
2026-09-15 07:29:57 +0200

    feat: add rule spa07
    Signed-off-by: Mats Johansson <extern.mats.johansson@digg.se>

--- src/rulesets/SpaRules.ts
+++ src/rulesets/SpaRules.ts
@@ -9,6 +9,12 @@ import { RuleExecutionContext } from '../util/RuleExecutionContext.js';
 
 const moduleName = 'SpaRules.js';
 
+const HTTP_METHODS = ['get', 'put', 'post', 'delete', 'patch', 'options', 'head', 'trace'];
+
+const TRACEPARENT_HEADER = 'traceparent';
+
+const ALTERNATIVE_TRACE_HEADERS = ['x-request-id'];
+
 export class Spa04 extends BaseRuleset {
   static customProperties: CustomProperties = {
     område: 'Spårbarhet',
@@ -22,7 +28,6 @@ export class Spa04 extends BaseRuleset {
   then = [
     {
       function: (targetVal: any, _opts: string, paths: string[]) => {
-        
         const header = targetVal?.headers?.traceparent;
 
         const hasTraceparent = header && (header.schema !== undefined || header.$ref !== undefined);
@@ -63,6 +68,82 @@ export class Spa04 extends BaseRuleset {
   severity = DiagnosticSeverity.Warning;
 }
 
+export class Spa07 extends BaseRuleset {
+  static customProperties: CustomProperties = {
+    område: 'Spårbarhet',
+    id: 'SPA.07',
+  };
+
+  message =
+    'Alternativa identifierare, såsom x-request-id, kan användas som komplement för interna behov, men SKALL INTE ersätta traceparent vid spårning av API-anrop mellan system och organisationer.';
+  given = '$.paths[*]';
+
+  then = [
+    {
+      function: (targetVal: any, _opts: string, context: { path: (string | number)[] }) => {
+        const results: { message: string; path: (string | number)[] }[] = [];
+
+        const pathParameters = Array.isArray(targetVal?.parameters) ? targetVal.parameters : [];
+
+        for (const method of HTTP_METHODS) {
+          const operation = targetVal?.[method];
+
+          if (!operation) {
+            continue;
+          }
+
+          const operationParameters = Array.isArray(operation.parameters) ? operation.parameters : [];
+          const headerNames = this.collectHeaderNames([...pathParameters, ...operationParameters]);
+
+          const usesAlternativeIdentifier = ALTERNATIVE_TRACE_HEADERS.some((header) => headerNames.has(header));
+
+          if (usesAlternativeIdentifier && !headerNames.has(TRACEPARENT_HEADER)) {
+            results.push({
+              message: this.message,
+              path: [...context.path, method],
+            });
+          }
+        }
+
+        return results;
+      },
+    },
+    {
+      function: (targetVal: string, _opts: string, paths: string[]) => {
+        this.trackRuleExecutionHandler(
+          JSON.stringify(targetVal, null, 2),
+          _opts,
+          paths,
+          this.severity,
+          this.constructor.name,
+          moduleName,
+          Spa07.customProperties,
+        );
+      },
+    },
+  ];
+
+  private collectHeaderNames(parameters: any[]): Set<string> {
+    const headerNames = new Set<string>();
+
+    for (const parameter of parameters) {
+      if (parameter?.in === 'header' && typeof parameter.name === 'string') {
+        headerNames.add(parameter.name.toLowerCase());
+      }
+    }
+
+    return headerNames;
+  }
+
+  constructor(context: RuleExecutionContext) {
+    super(context);
+    super.initializeFormats(['OAS3']);
+  }
+
+  severity = DiagnosticSeverity.Error;
+}
+
 export default {
   Spa04,
+  Spa07,
 };
